08f5154457b172bab82482a5e2d976334642bc18
annabelng <a.h.ng952@gmail.com>
2022-10-15 23:34:19 -0700

    update train

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import cohere
-import numpy as np 
+import numpy as np
 
 from cohere.classify import Example
 
@@ -7,8 +7,8 @@ co = cohere.Client('sxxgeiXaO1f9aVYZGBGocCq0pd5R85VcvTNb6ZkG')
 
 response = co.classify(
   model='b73cb43c-740e-4f98-a5c1-90586bbf65e8-ft',
-  inputs=["I drank a lot of water and went on a long hike and watched some birds"])
+  inputs=["I hate and love myself but i am so tired"])
 
 predictions = [c.prediction for c in response.classifications]
 
-print('The main emotion of this entry is: ' + predictions[0])
+print('The main emotion  of this entry is: ' + predictions[0])
