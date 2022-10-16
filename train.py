118183addc3723daa68f68dd9a774ab2f869b433
annabelng <a.h.ng952@gmail.com>
2022-10-15 21:25:35 -0700

    update train

--- train.py
+++ train.py
@@ -1,9 +1,15 @@
 import cohere
+import numpy as np 
+
 from cohere.classify import Example
-co = cohere.Client('b73cb43c-740e-4f98-a5c1-90586bbf65e8-ft')
+
+co = cohere.Client('sxxgeiXaO1f9aVYZGBGocCq0pd5R85VcvTNb6ZkG')
+
 
 response = co.classify(
   model='b73cb43c-740e-4f98-a5c1-90586bbf65e8-ft',
   inputs=["I drank a lot of water and went on a long hike and watched some birds"])
 
-print('The confidence levels of the labels are: {}'.format(response.classifications))
+predictions = [c.prediction for c in response.classifications]
+
+print('The main emotion of this entry is: ' + predictions[0])
