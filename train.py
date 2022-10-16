21fbf3326067b7ca0cea8b085cdca8b47d8da301
annabelng <a.h.ng952@gmail.com>
2022-10-15 20:54:51 -0700

    update models

--- train.py
+++ train.py
@@ -1,4 +1,9 @@
 import cohere
-co = cohere.Client(api_key)
+from cohere.classify import Example
+co = cohere.Client('b73cb43c-740e-4f98-a5c1-90586bbf65e8-ft')
 
+response = co.classify(
+  model='b73cb43c-740e-4f98-a5c1-90586bbf65e8-ft',
+  inputs=["I drank a lot of water and went on a long hike and watched some birds"])
 
+print('The confidence levels of the labels are: {}'.format(response.classifications))
