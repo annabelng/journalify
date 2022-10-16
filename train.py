20a2d89b288d487c0391907402d9b783b11d83ea
annabelng <a.h.ng952@gmail.com>
2022-10-15 21:40:24 -0700

    update train

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ from cohere.classify import Example
 
 co = cohere.Client('sxxgeiXaO1f9aVYZGBGocCq0pd5R85VcvTNb6ZkG')
 
-
 response = co.classify(
   model='b73cb43c-740e-4f98-a5c1-90586bbf65e8-ft',
   inputs=["I drank a lot of water and went on a long hike and watched some birds"])
